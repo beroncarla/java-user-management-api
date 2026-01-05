bc80a0a534e2bd8f01ccaf12ba760902dfb54372
beroncarla <beroncarlaviedma@gmail.com>
2026-01-04 22:38:50 -0300

    create user class

--- src/main/java/com/example/usermanagement/model/User.java
+++ src/main/java/com/example/usermanagement/model/User.java
@@ -1,4 +1,14 @@
 package com.example.usermanagement.model;
 
-public class User {
+/**
+ * Modelo de dominio
+ * Representa un usuario del sistema externo.
+ * <p>
+ * En integraciones, este objeto será serializado a JSON.
+ */
+public record User(
+        Long id,
+        String name,
+        String email
+) {
 }
