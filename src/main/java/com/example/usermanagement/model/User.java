bfb369bbc68e51b35ace35803e3be01cf30d8014
beroncarla <beroncarlaviedma@gmail.com>
2026-01-05 13:53:41 -0300

    modify User, add jakarta validation

--- src/main/java/com/example/usermanagement/model/User.java
+++ src/main/java/com/example/usermanagement/model/User.java
@@ -1,14 +1,22 @@
 package com.example.usermanagement.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 /**
  * Modelo de dominio
  * Representa un usuario del sistema externo.
  * <p>
  * En integraciones, este objeto será serializado a JSON.
+ * Validaciones con Jakarta Validation API.
+ * NotNull: el campo no puede ser null
+ * NotBlank: el campo no puede ser null ni vacio
+ * Email: el campo debe tener formato de email valido
  */
 public record User(
-        Long id,
-        String name,
-        String email
+        @NotNull Long id,
+        @NotBlank String name,
+        @Email String email
 ) {
 }
