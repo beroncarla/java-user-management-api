54d6eb36fb485e7492ba961f39f2b2536b32aafe
beroncarla <beroncarlaviedma@gmail.com>
2026-01-05 13:28:48 -0300

    create Application

--- src/main/java/com/example/usermanagement/Application.java
+++ src/main/java/com/example/usermanagement/Application.java
@@ -1,4 +1,15 @@
 package com.example.usermanagement;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Punto de entrada de la aplicacion Spring Boot
+ */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
