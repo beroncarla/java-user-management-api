495deb01de5dfd0ee76d92ccebb91eeb11120d0a
beroncarla <beroncarlaviedma@gmail.com>
2026-01-05 14:02:49 -0300

    create package config and class OpenApiConfig

---
+++ src/main/java/com/example/usermanagement/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.usermanagement.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Java User Management API")
+                        .version("1.0")
+                        .description("API REST para integraciones SAP BTP / CPI"));
+    }
+}
