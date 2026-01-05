6ad996e6aba83901954a2387a65700d441886ee7
beroncarla <beroncarlaviedma@gmail.com>
2026-01-05 12:27:40 -0300

    create UserRepository

--- src/main/java/com/example/usermanagement/repository/UserRepository.java
+++ src/main/java/com/example/usermanagement/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.usermanagement.repository;
 
+import com.example.usermanagement.model.User;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
 /**
  * Simula una fuente de datos externa.
  * En el mundo real podria ser:
@@ -8,4 +14,18 @@ package com.example.usermanagement.repository;
  * - un servicio remoto
  */
 public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+    public UserRepository(){
+        this.users.add(new User(1L, "Alice", "alice@gmail.com");
+        this.users.add(new User(2L, "Bob", "bob@gmail.com");
+    }
+    public List<User> findAll() {
+        return users;
+    }
+    public Optional<User> findById(Long id){
+        return this.users.stream()
+                .filter(user -> user.id().equals(id))
+                .findFirst();
+    }
 }
