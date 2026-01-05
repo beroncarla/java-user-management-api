e44f1f532f1df3a248f5c17ba3008f2881cf7233
beroncarla <beroncarlaviedma@gmail.com>
2026-01-04 22:53:16 -0300

    coment in UserRepository

--- src/main/java/com/example/usermanagement/repository/UserRepository.java
+++ src/main/java/com/example/usermanagement/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.usermanagement.repository;
 
+/**
+ * Simula una fuente de datos externa.
+ * En el mundo real podria ser:
+ * - una base de datos
+ * - otro sistema
+ * - un servicio remoto
+ */
 public class UserRepository {
 }
